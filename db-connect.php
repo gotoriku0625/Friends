6f4e9e67bfbf4c3c3c5ec38bd8003a1c0f69f510
gotoriku0625 <2201147@s.asojuku.ac.jp>
2024-04-19 10:41:41 +0900

    a

---
+++ db-connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql301.phy.lolipop.lan';
+    const DBNAME = 'LAA1517793-friends';
+    const USER = 'LAA1517801';
+    const PASS = 'pass0625';
+ 
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
