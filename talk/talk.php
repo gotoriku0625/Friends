adb830bd5f4c90cc91ce028f6d5a806fb8e44397
koko2025 <sahakoke@icloud.com>
2024-05-13 09:59:28 +0900

    talk③

--- talk/talk.php
+++ talk/talk.php
@@ -5,10 +5,14 @@
     <meta charset="UTF-8">
     <title>トーク</title>
     <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="css/fontawesome-free-5.15.3-web/css/all.min.css">
     <script src=""></script>
 </head>
 <body>
     <div class="talk-system">
-        <form class="send-box flex-box" action="">
+        <form class="send-box flex-box" action="" method="post">
+            <textarea id="textarea" type="text" name="text" rows="1" required placeholder="message"></textarea>
+            <input type="submit" name="submit" value="送信" id="send">
+            <label for="send"><i class=""
 </body>
 </html>
