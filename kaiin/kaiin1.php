5d70168b1cfd784b2ac9cead1a2f43c61103f68c
Shugo0611 <2201171@s.asojuku.ac.jp>
2024-05-17 10:09:28 +0900

    １

---
+++ kaiin/kaiin1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+新規会員登録<br>
+<hr>
+<form action=""method="post">
+ユーザー名<br>
+<input type="text" name="name">
+<br>
+<br>
+ID/メールアドレス<br>
+<input type="text" name="mail">
+<br>
+<br>
+パスワード<br>
+<input type="text" name="pass">
+    <button type="submit">次へ</button>
+</body>
+</html>
