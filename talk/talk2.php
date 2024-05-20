e61d3ab17fd0b6c4e6f51ba9b3b5c761e350f5f1
koko2025 <sahakoke@icloud.com>
2024-05-20 12:34:20 +0900

    talk

--- talk/talk2.php
+++ talk/talk2.php
@@ -3,7 +3,7 @@
 <?php
 $current_user = get_user($_SESSION['user_id']);
 $destination_user = get_user($_GET['user_id']);
-$messages = get_messages($current_user['id'],$destination_user['id']);
+$messages = get_messages($current_user['id'],$reciver['id']);
 
 $pdo=new PDO($connect,USER,PASS);
 $imagePath='select icon_image from user,profile where user.user_id=profile.user_id and profile.user_id = :user_id';
@@ -11,24 +11,40 @@ $sql=$pdo->prepare($imagePath);
 ?>
 
 <body>
-   <div class="message"> 
-    <?php foreach ($messages as $message) :?>
-        <div class="my_message">
-        <?php if($message['user_id']==$current_user['id']):?>
-            <?php $sql->bindParam(':user_id',$current_user,PDO::PARAM_STR); ?>
-            <div class="mycomment right">
-                <p><?php $message['text']?></p><img src="../user_image/main<?php $sql->execute();?>" class="message_user_img">
-            </div>
-        <?php else :?>
-            <?php $sql->bindParam(':user_id',$destination_user,PDO::PARAM_STR); ?>
-            <div class="left">
-                <img src="../user_image/main<?php $sql->execute();?>" class="message_user_img">
-                <div class="says">
-                    <?php $message['text']?>
+    <div class="message"> 
+        <?php foreach ($messages as $message){
+            echo '<div class="my_message">';
+            if($message['user_id']==$current_user['id']){
+            $sql->bindParam(':user_id',$current_user,PDO::PARAM_STR);
+                echo<<<EOF
+                <div class="mycomment right">
+                    <p>$message['text']</p><img src="../user_image/main/$sql->execute()" class="message_user_img">
                 </div>
-        <?php endif;?>
-            </div>
-    <?php endforeach?>
-        
+                EOF;
+            }else{
+                $sql->bindParam(':user_id',$reciver,PDO::PARAM_STR);
+                echo<<<EOF
+                <div class="left">
+                    <img src="../user_image/main/$sql->execute()" class="message_user_img">
+                    <div class="says">
+                        $message['text']
+                    </div>
+                </div>
+                EOF;
+            }
+        }?>
+
+        <div class="message_process">
+            <h2 class="message_title">メッセージ</h2>
+            <form method="post" action="talk.php#talk-area">
+            <textarea id="textarea" type="text" name="text" rows="1" required placeholder="message"></textarea>
+                <input type="hidden" name="reciver_id" value="<?= $reciver_user['id'] ?>">
+                <div class="message_btn">
+                    <button class="btn btn-outline-primary" type="submit" name="post" value="post" id="post">送信</button>
+                </div>
+            </form>
+        </div>
+    </div>   
 </body>
 </html>
+
