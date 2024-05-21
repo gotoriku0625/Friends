fe633eea6534d2dcebc518b083bf49cc67a32ce0
koko2025 <sahakoke@icloud.com>
2024-05-21 11:52:21 +0900

    talk㉑

--- talk/function.php
+++ talk/function.php
@@ -1,6 +1,8 @@
 <?php
+$pdo=new PDO($connect,USER,PASS);
 function get_user($user_id){
     try{
+        // ユーザ情報取得
         $user='select user_id,user_name,nick_name,icon_image,gender 
               from user u,profile p 
               where u.user_id=p.user_id and u.user_id=:id';
@@ -15,11 +17,12 @@ function get_user($user_id){
 
 function get_talks($sender_id,$reciver_id){
     try{
+        // トークのユーザー同士の情報を取得する
         $talk='select * from talk 
                where (sender_id = :id and reciver_id = :reciver_id) or (sender_id = :reciver_id and reciver_id = :id)
                ORDER BY created_at ASC';
         $sql=$pdo->prepare($talk);
-        $sql->execute(array(':id'=>$sender_id,':reciver_id'=>$reciver));
+        $sql->execute(array(':id'=>$user_id,':reciver_id'=>$reciver));
         return $sql->fetchAll();
     }catch(\Exception $e){
         error_log('エラー発生：'.$e->getMessage());
--- talk/talk2.php
+++ talk/talk2.php
@@ -1,43 +1,33 @@
 <?php require '../db-connect.php';?>
 <?php require 'function.php';?>
 <?php
-$current_user = get_user($_SESSION['user_id']);
-$destination_user = get_user($_GET['user_id']);
-$messages = get_messages($current_user['id'],$reciver['id']);
-
-$pdo=new PDO($connect,USER,PASS);
-$imagePath='select icon_image from user,profile where user.user_id=profile.user_id and profile.user_id = :user_id';
-$sql=$pdo->prepare($imagePath);
+$current_user = get_user($_SESSION['user_id']);// 現在ログインしているユーザー情報
+$reciver = get_user($_GET['user_id']);// トーク相手のユーザー情報
+$messages = get_messages($current_user['id'],$reciver['id']);// やり取りされるメッセージ情報
 ?>
 
 <body>
     <div class="message"> 
+        <h2 class="center"><?= $reciver['user_name']?> </h2>
         <?php foreach ($messages as $message){
             echo '<div class="my_message">';
             if($message['user_id']==$current_user['id']){
-            $sql->bindParam(':user_id',$current_user,PDO::PARAM_STR);
-                echo'<div class="mycomment right">';
-                    <p>.$message['text'].</p><img src="../user_image/main/.$sql->execute()." class="message_user_img">
-                </div>
-                EOF;
+                echo'<div class="mycomment right">';//　自分のメッセージ表示部分↓
+                    echo '<p>'.$message['text'].'</p><img src="../user_image/main/'.$current_user['icon_image'].'" class="message_user_img">';
+                echo'</div>';
             }else{
-                $sql->bindParam(':user_id',$reciver,PDO::PARAM_STR);
-                echo<<<EOF
-                <div class="left">
-                    <img src="../user_image/main/$sql->execute()" class="message_user_img">
-                    <div class="says">
-                        $message['text']
-                    </div>
-                </div>
-                EOF;
+                echo '<div class="left">';// 相手のメッセージ表示部分↓
+                    echo '<img src="../user_image/main/'.$reciver['icon_image'].'" class="message_user_img">';
+                    echo '<div class="says">'.$message['text'].'</div>';
+                echo '</div>';
             }
         }?>
 
         <div class="message_process">
             <h2 class="message_title">メッセージ</h2>
             <form method="post" action="talk.php#talk-area">
-            <textarea id="textarea" type="text" name="text" rows="1" required placeholder="message"></textarea>
-                <input type="hidden" name="reciver_id" value="<?= $reciver_user['id'] ?>">
+            <textarea id="textarea from-control" type="text" name="text" rows="1" required placeholder="message"></textarea>
+                <input type="hidden" name="reciver_id" value="<?= $reciver['id'] ?>">
                 <div class="message_btn">
                     <button class="btn btn-outline-primary" type="submit" name="post" value="post" id="post">送信</button>
                 </div>
