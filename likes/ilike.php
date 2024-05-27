809e9e80f12953398666dbca64d0381d4cd2383c
gotoriku0625 <2201147@s.asojuku.ac.jp>
2024-05-27 10:46:10 +0900

    a

--- likes/ilike.php
+++ likes/ilike.php
@@ -1,4 +1,11 @@
-
+<?php
+    const SERVER = 'mysql301.phy.lolipop.lan';
+    const DBNAME = 'LAA1517801-friends';
+    const USER = 'LAA1517801';
+    const PASS = 'pass0625';
+ 
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
