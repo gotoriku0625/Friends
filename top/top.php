03c62d893ec3d34f8c6e6430d9a2f3e4b64d2b6c
miyuu22 <139205386+miyuu22@users.noreply.github.com>
2024-06-04 12:02:32 +0900

    menu 挿入

--- top/top.php
+++ top/top.php
@@ -1,29 +1,9 @@
-<!DOCTYPE html>
-<html lang="ja">
+<?php require '../menu/menu.html';?>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../menu/menu.css">
-    <title>top</title>
 </head>
 
 <body>
-    <div class="menu">
-        <div class="logo-space">
-            <a href="../top/top.html"><img src="../image/logo.png" class="logo"></a>
-        </div>
-        <div class="icon"></div>
-        <!-- バックエンドの方、ユーザーネームの出力お願いします -->
-        <div class="name">ユーザー名</div>
-        <div class="link-space">
-            <p><img src="../menu-image/parson-free-icon.png" class="parson-free-icon"><a href="../profile/profile.html">プロフィール</a></p>
-            <p><img src="../menu-image/seach-free-icon.png" class="seach-free-icon"><a href="../profile/search.php">さがす</a></p>
-            <p><img src="../menu-image/like-free-icon.png" class="like-free-icon"><a href="../profile/ilike.php">いいね</a></p>
-            <p><img src="../menu-image/community-free-icon.png" class="community-free-icon"><a href="../profile/community.php">コミュニティ</a></p>
-            <p><img src="../menu-image/talk-free-icon.png" class="talk-free-icon"><a href="../profile/talk.php">トーク</a></p>
-        </div>
-    </div>
-
     <div class="main">
         <?php
         $pdo=new PDO($connect,USER,PASS);
