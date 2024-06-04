e05b8c65a0fcaabcb15883784a621fec4abb7955
harada93 <148171467+harada93@users.noreply.github.com>
2024-06-04 12:27:44 +0900

    unei

---
+++ manajement/management_login.php
@@ -0,0 +1,55 @@
+<?php session_start(); ?>
+<?php require '../db-connect.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<div class="container">
+    <div class="header">
+        <img src="../image/logo.png" alt="Logo" class="logo">
+    </div>
+    <div class="content">
+        <img src="../image/person1.png" alt="human1" class="side-image">
+        <form action="login.php" method="post" class="login-form">
+            <div class="form-group">
+                <label for="id">E-mail</label>
+                <input type="text" id="id" name="id">
+            </div>
+            <div class="form-group">
+                <label for="password">Pass</label>
+                <input type="password" id="password" name="password">
+            </div>
+            <button type="submit" name="login" value="ログイン">ログイン</button>
+        </form>
+        <img src="../image/person2.png" alt="human2" class="side-image">
+    </div>
+    <div class="footer">
+    </div>
+</div>
+</body>
+</html>
+<?php
+    $pdo=new PDO($connect,USER,PASS);
+    $sql=$pdo->prepare('select user_id from user where mail=?');
+    if(isset($_POST['login']) && $_POST['login'] === "ログイン"){
+        $sql->execute([$_POST['id']]);
+        foreach($sql as $row){
+            if(password_verify($_POST['password'],$row['pass']) == true){//ハッシュ化したパスワードと一致しているか
+                $_SESSION['user_id']=$row['user_id'];
+            }
+        }
+        if(isset($_SESSION['user_id'])){
+            echo <<<EOS
+            \n
+            EOS;
+        }else{
+            echo '<p>ログイン名またはパスワードが違います。</p>';
+        }
+        header('Location:./login.php');
+        exit;
+    }
+?>
