5a413cfa630ddf64e014c4828cf13ebff7434c63
Shugo0611 <2201171@s.asojuku.ac.jp>
2024-06-05 12:45:13 +0900

    プロフィールjacascript変更

--- profile/js/script.js
+++ profile/js/script.js
@@ -1,52 +1,18 @@
-document.getElementById('imageInput').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const image = document.getElementById('selectedImage');
-            image.src = e.target.result;
-            image.style.display = 'block';
-        };
-        reader.readAsDataURL(file);
-    }
-});
-
-document.getElementById('subImageInput1').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const image = document.getElementById('subImage1');
-            image.src = e.target.result;
-            image.style.display = 'block';
-        };
-        reader.readAsDataURL(file);
-    }
-});
-
-document.getElementById('subImageInput2').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const image = document.getElementById('subImage2');
-            image.src = e.target.result;
-            image.style.display = 'block';
-        };
-        reader.readAsDataURL(file);
-    }
-});
-
-document.getElementById('subImageInput3').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const image = document.getElementById('subImage3');
-            image.src = e.target.result;
-            image.style.display = 'block';
-        };
-        reader.readAsDataURL(file);
-    }
-});
-
+function Change(inputId, imageId) {
+    document.getElementById(inputId).addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                const image = document.getElementById(imageId);
+                image.src = e.target.result;
+                image.style.display = 'block';
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+}
+Change('imageInput','selectedImage');
+Change('subImageInput1', 'subImage1');
+Change('subImageInput2', 'subImage2');
+Change('subImageInput3', 'subImage3');
