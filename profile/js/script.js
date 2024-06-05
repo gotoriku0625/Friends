fea70cd43a510d1a7d21774af17f2060566a0d44
Shugo0611 <2201171@s.asojuku.ac.jp>
2024-06-05 12:36:27 +0900

    プロフィールサブ画像

--- profile/js/script.js
+++ profile/js/script.js
@@ -11,4 +11,42 @@ document.getElementById('imageInput').addEventListener('change', function(event)
     }
 });
 
+document.getElementById('subImageInput1').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const image = document.getElementById('subImage1');
+            image.src = e.target.result;
+            image.style.display = 'block';
+        };
+        reader.readAsDataURL(file);
+    }
+});
+
+document.getElementById('subImageInput2').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const image = document.getElementById('subImage2');
+            image.src = e.target.result;
+            image.style.display = 'block';
+        };
+        reader.readAsDataURL(file);
+    }
+});
+
+document.getElementById('subImageInput3').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const image = document.getElementById('subImage3');
+            image.src = e.target.result;
+            image.style.display = 'block';
+        };
+        reader.readAsDataURL(file);
+    }
+});
 
