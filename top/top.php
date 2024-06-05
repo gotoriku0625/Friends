457c89dedf5247dd402555f173df53b86a82f054
koko2025 <sahakoke@icloud.com>
2024-06-05 12:14:33 +0900

    top

--- top/top.php
+++ top/top.php
@@ -13,7 +13,7 @@
         $sql=$pdo->prepare('select user.user_id,icon_image,nick_name,gender,age from user,profile,hobby
                             where profile.hobby_id=hobby.hobby_id
                             and profile.hobby_id=profile.hobby_id
-                            and user.user_id!=?
+                            and user.user_id<>?
                             order by user.user_id
                             limit 10');
         $sql->execute($_SESSION['user_id']);
