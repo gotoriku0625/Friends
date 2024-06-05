f0a36d2c35dfb31c426c9a1290dffe19df4c5e86
gotoriku0625 <2201147@s.asojuku.ac.jp>
2024-06-05 12:14:51 +0900

    いいね機能

--- likes/ilike.php
+++ likes/ilike.php
@@ -33,6 +33,10 @@ while ($row = $result->fetch_assoc()) {
 $stmt->close();
 $conn->close();
 ?>
+<?php require '../menu/menu.html';?>
+<head>
+    <link rel="stylesheet" href="../menu/menu.css">
+</head>
 
 <!DOCTYPE html>
 <head>
@@ -43,23 +47,6 @@ $conn->close();
 </head>
 
 <body>
-    <div class="menu">
-        <div class="logo-space">
-            <a href="../top/top.html"><img src="../image/logo.png" class="logo"></a>
-        </div>
-        <div class="icon"></div>
-        <!-- バックエンドの方、ユーザーネームの出力お願いします -->
-        <div class="name">ユーザー名</div>
-        <div class="link-space">
-            <p><img src="../menu-image/parson-free-icon.png" class="parson-free-icon"><a href="../profile/profile.html">プロフィール</a></p>
-            <p><img src="../menu-image/seach-free-icon.png" class="seach-free-icon"><a href="../search/search.php">さがす</a></p>
-            <p><img src="../menu-image/like-free-icon.png" class="like-free-icon"><a href="../likes/ilike.php">いいね</a></p>
-            <p><img src="../menu-image/talk-free-icon.png" class="talk-free-icon"><a href="../talk/talk.php">トーク</a></p>
-            <p><img src="../menu-image/community-free-icon.png" class="community-free-icon"><a href="../community/community.php">コミュニティ</a></p>
-            
-        </div>
-    </div>
-
     <div class="main">
     いいねした人<img src="../menu-image/like-free-icon.png" class=""><button onclick="location.href='./youlike.php'">あなたへいいね</button><img src="../image/unlike.svg" class="">
     <hr></hr>
@@ -76,15 +63,14 @@ $conn->close();
     </style>
 </head>
 <body>
-    <h1>いいねした人の一覧</h1>
-    <?php if (!empty($liked_users)): ?>
+    <?php if (!empty($liked_user)): ?>
         <ul class="user-list">
-            <?php foreach ($liked_users as $user): ?>
+            <?php foreach ($liked_user as $user): ?>
                 <li><?php echo htmlspecialchars($user['user_name'], ENT_QUOTES, 'UTF-8'); ?></li>
             <?php endforeach; ?>
         </ul>
     <?php else: ?>
-        <p>いいねした人はいません。</p>
+        <p>友達になりたい人を見つけに行きましょう。<img src="../image/person1.png"></p>
     <?php endif; ?>
 </body>
 </html>
