3053623b1e21c6c8ca5c0f0b9a0c5cc26722b3a8
miyuu22 <139205386+miyuu22@users.noreply.github.com>
2024-06-06 22:20:16 +0900

    top

--- top/top.php
+++ top/top.php
@@ -37,7 +37,7 @@
                 echo '<a href="../profile/profile.php"><img src="',$row['icon_image'],'"</a>';// アイコン
             }
             // アイコンとユーザー名、年齢を表示
-            echo '<p>',$row['nick_name'];// ユーザー名
+            echo '<p class="nick_name1">',$row['nick_name'];// ユーザー名
             echo '(',$row['age'],')</p>';// 年齢
         }
         echo '</div>';
@@ -62,7 +62,7 @@
                 echo '<a href="../profile/profile.php"><img src="',$row['icon_image'],'"</a>';// アイコン
             }
             // アイコンとユーザー名、年齢を表示
-            echo '<p>',$row['nick_name'];// ユーザー名
+            echo '<p class="nick_name2">',$row['nick_name'];// ユーザー名
             echo '(',$row['age'],')</p>';// 年齢
         }
         
