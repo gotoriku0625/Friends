49f5be3b076139679a3b18ece3ba7d9ecf82d270
koko2025 <sahakoke@icloud.com>
2024-06-07 17:24:24 +0900

    追加

--- header.php
+++ header.php
@@ -7,6 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- reset.css destyle -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
-    <link rel="stylesheet" href="menu/menu.css">
-    <title>menu</title>
-</head>
+    <link rel="stylesheet" href="../menu/menu.css">
+<!-- headの閉じタグは各ページに追加する（titleタグも追加で）
+参考はtop.phpを見てください -->
