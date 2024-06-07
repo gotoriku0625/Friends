d6facd9bbfff7eb06dd6e9ac63c14d136e88957e
koko2025 <sahakoke@icloud.com>
2024-06-07 16:05:52 +0900

    menu/headの部分を分離した

--- menu/menu.php
+++ menu/menu.php
@@ -1,16 +1,4 @@
-<?php session_start(); ?>
-<?php require '../db-connect.php';?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- reset.css destyle -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
-    <link rel="stylesheet" href="menu.css">
-    <title>menu</title>
-</head>
-
+<?php require '../header.php';?>
 <body>
     <div class="menu">
         <div class="logo-space">
