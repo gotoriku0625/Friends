61bdd64239176fa36dccad9cf25c10aa40379dee
koko2025 <sahakoke@icloud.com>
2024-06-07 23:02:01 +0900

    編集

--- header.php
+++ header.php
@@ -1,7 +1,5 @@
 <?php session_start(); ?>
 <?php require '../db-connect.php';?>
-<!DOCTYPE html>
-<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
