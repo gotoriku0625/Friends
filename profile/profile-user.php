b64722599e0364738c75a7d37d46185d763b4d68
miyuu22 <139205386+miyuu22@users.noreply.github.com>
2024-06-07 09:30:38 +0900

    pro

---
+++ profile/profile-user.php
@@ -0,0 +1,13 @@
+<?php session_start(); ?>
+<?php require '../db-connect.php';?>
+<?php require '../menu/menu.html';?>
+<head>
+    <link rel="stylesheet" href="../menu/menu.css">
+    <link rel="stylesheet" href="top.css">
+</head>
+
+<body>
+    <div class="main">
+
+    </div>
+</body>
