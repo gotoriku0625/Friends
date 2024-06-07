b34f65fbd9da159a46775adb359618c601e9fe90
miyuu22 <139205386+miyuu22@users.noreply.github.com>
2024-06-07 11:34:51 +0900

    top

--- top/top.php
+++ top/top.php
@@ -62,7 +62,7 @@
                 echo '<a href="../profile/profile-user.php"><img src="',$row['icon_image'],'"</a>';// アイコン
             }
             // アイコンとユーザー名、年齢を表示
-            echo '<p class="nick_name2">',$row['username'];// ユーザー名
+            echo '<p class="nick_name2">',$row['user_name'];// ユーザー名
             echo '(',$row['age'],')</p>';// 年齢
         }
         
