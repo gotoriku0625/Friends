0558de8c206130d28494a8b97efe89bf9344d805
koko2025 <sahakoke@icloud.com>
2024-06-11 12:15:34 +0900

    talkスクロール

--- talk/talk2.php
+++ talk/talk2.php
@@ -20,7 +20,6 @@ try{
         $talk_text=$_POST['text'];
         $user_id=$_SESSION['user1_id'];
         $reciver_id=$_POST['reciver_id'];
-        
 
         $talk_text=htmlspecialchars($talk_text,ENT_QUOTES,'UTF-8');
         $user_id=htmlspecialchars($user_id,ENT_QUOTES,'UTF-8');
@@ -44,17 +43,42 @@ try{
         }
         echo <<<EOS
         <script>
-        location.href='https://aso2201147.tonkotsu.jp/Friends/talk/talk2.php';
+        window.onload = function() {
+            var form = document.getElementById('form');
+            var container = document.getElementById('container');
+                
+            // 下までスクロールする
+            var scrollToBottom = () => {
+                container.scrollTop = container.scrollHeight;
+            };
+            
+            // 一番下までスクロールしているかどうか
+            var isScrollBottom = () => {
+                return container.scrollHeight === container.scrollTop + container.offsetHeight;
+            };
+            
+            form.addEventListener('submit', (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+            // 一番下までスクロールされていれば追加後も一番下までスクロールする
+                if (isScrollBottom()) {
+                    scrollToBottom();
+                }
+                // 一番下までスクロールされていなければスクロールしない
+                else {
+                }
+            });
+        };
         </script> 
         \n
         EOS;
+        header("Location: " . $_SERVER['PHP_SELF']);
     }
 }catch(Exception $e){
     echo 'ただいま障害により大変ご迷惑をおかけしております。';
     exit();
 }
 ?>
-
 <body>
 <?php require '../menu/menu.php';?>
     <div id="main" class="main"> 
@@ -75,15 +99,16 @@ try{
                     if($message['sender_id']==$current_user['user_id']){
                         echo'<div class="comment right">';//　自分のメッセージ表示部分↓
                             echo '<div class="talking">';
-                                echo '<div class="content">'.$message['content'].'</div>';
+                                echo '<div class="content">'.nl2br($message['content']).'</div>';
                         echo'</div></div><div class="bms_clear"></div>'; //回り込みを解除（スタイルはcssで充てる)
                     }else{
                         echo '<div class="comment left">';// 相手のメッセージ表示部分↓
                             // echo '<img src="../user_image/main/'.$reciver['icon_image'].'" class="message_user_img">';
-                            echo '<div class="talking"><div class="content">'.$message['content'].'</div></div>';
+                            echo '<div class="talking"><div class="content">'.nl2br($message['content']).'</div></div>';
                         echo '</div><div class="bms_clear"></div>';
                     }
                 }
+                echo '<div id="container" class="container"></div>';
                 echo '</div>';
             }
             ?>
@@ -98,44 +123,3 @@ try{
         </div>
     </div>   
 </body>
-
-<?php
-// try{
-//     if(isset($_POST['post'])&&$_POST['post']==='submit'){
-//     $talk_text=$_POST['text'];
-//     $user_id=$_SESSION['user1_id'];
-//     $reciver_id=$_POST['reciver_id'];
-    
-
-//     $talk_text=htmlspecialchars($talk_text,ENT_QUOTES,'UTF-8');
-//     $user_id=htmlspecialchars($user_id,ENT_QUOTES,'UTF-8');
-
-//     $pdo=new PDO($connect,USER,PASS);
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-//     $add='insert into talk(sender_id,reciver_id,content) values (?,?,?)';
-//     $sql=$pdo->prepare($add);
-
-//     $data[] = $user_id;
-//     $data[] = $reciver_id;
-//     $data[] = $talk_text;
-
-//     $sql->execute($data);
-//     $pdo=null;
-
-//     if(!check_relation_talk($user_id,$reciver_id)){
-//         $member_add='insert into talk_member(sender_id,reciver_id) values (?,?)';
-//         $sql=$pdo->prepare($member_add);
-//         $sql->execute($user_id,$reciver_id);
-//     }
-//     header('Location:https://aso2201147.tonkotsu.jp/Friends/talk/talk2.php');
-// }
-//     // header('Location:https://aso2201147.tonkotsu.jp/Friends/talk/talk2.php');
-//     // $rel = $_GET['reload'];
-//     // if ($rel == 'true') {
-//     //   header("Location: " . $_SERVER['PHP_SELF']);
-//     // }
-// }catch(Exception $e){
-//     echo 'ただいま障害により大変ご迷惑をおかけしております。';
-//     exit();
-// }
-?>
