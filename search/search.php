6357feb01b8c9df85441f319c54965b93d503475
miyuu22 <139205386+miyuu22@users.noreply.github.com>
2024-06-17 09:41:06 +0900

    seach

--- search/search.php
+++ search/search.php
@@ -1,9 +1,11 @@
-<?php require '../db-connect.php';?>
-<?php require '../menu/menu.html';?>
-<head>
-    <link rel="stylesheet" href="../menu/menu.css">
-    <link rel="stylesheet" href="search.css">
+<?php require '../header.php';?>
+    <link rel="stylesheet" href="../search.css">
+    <title>検索</title>
 </head>
+
+<body>
+<?php require '../menu/menu.php';?>
+    <div class="main"></div>
 
 <?php $pdo = new PDO($connect, USER, PASS);
 
@@ -37,14 +39,6 @@ $prefectures = [
     "宮崎県", "鹿児島県", "沖縄県"
 ];
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>ユーザー検索</title>
-    
-</head>
-<body>
     <div class="container">
         <h1>ユーザー検索</h1>
 
