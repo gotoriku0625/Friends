f0ee89bf1742aa1ffecf7b8885641a0324fa9874
miyuu22 <139205386+miyuu22@users.noreply.github.com>
2024-06-19 10:10:36 +0900

    talk

--- talk/talk_top.php
+++ talk/talk_top.php
@@ -22,7 +22,7 @@
                 }
                 $bottom_talk = get_bottom_talk($current_user['user_id'],$reciver['user_id']);
         echo '<div class="talk-head">';
-        echo '<span>トーク</span>';
+        echo '<h1 class="h1-talk">トーク</s>';
         echo '<hr></div>';
         echo '<div class="row">';
             echo '<div class="col-8 offset-2">';
