ed985c1d5558a5915d8d126c8a97c5bd59da1179
koko2025 <sahakoke@icloud.com>
2024-06-19 12:28:28 +0900

    profile/とりあえずコードは書けました

--- profile/profile-output.php
+++ profile/profile-output.php
@@ -24,12 +24,14 @@
         $fileType_main = pathinfo($path,PATHINFO_EXTENSION);
             
         // アイコンをサーバーのフォルダに送信
-        if(!empty($_FILES['subPhoto1']['name'])){
+        if(!empty($_FILES['profileIcon']['name'])){
             $allowTypes = array('jpg','png','jpeg','gif');
             if(in_array($fileType,$allowTypes)){
                 if(move_uploaded_file($_FILES['profileIcon']['tmp_name'],"../". $path)){
-                    if (exif_imagetype("../".$path)) {//画像ファイルかのチェック
-                    } else {
+                    if (!exif_imagetype("../".$path)) {//画像ファイルかのチェック
+                        if (!empty($_SERVER['HTTP_REFERER'])){
+                            header("Location:". $_SERVER['HTTP_REFERER']);
+                        }
                     }
                 }
             }
@@ -42,14 +44,14 @@
             $subpath1 = $sub . $fileName_sub1;
             $fileType_sub1 = pathinfo($subpath1,PATHINFO_EXTENSION);
             // 画像をサーバーにアップロード
-            if(!empty($_FILES['subPhoto1']['name'])){
+            if(!empty($_FILES[$subImg[0]]['name'])){
                 $allowTypes = array('jpg','png','jpeg','gif');
                 if(in_array($fileType,$allowTypes)){
-                    if(move_uploaded_file($_FILES['profileIcon']['tmp_name'],"../". $path)){
-                        if (exif_imagetype("../".$path)) {//画像ファイルかのチェック
-                            
-                        } else {
-
+                    if(move_uploaded_file($_FILES[$subImg[0]]['tmp_name'],"../". $path)){
+                        if (!exif_imagetype("../".$path)) {//画像ファイルかのチェック
+                            if (!empty($_SERVER['HTTP_REFERER'])){
+                                header("Location:". $_SERVER['HTTP_REFERER']);
+                            }
                         }
                     }
                 }
@@ -62,15 +64,15 @@
             $subpath2 = $sub . $fileName_sub2;
             // 画像パスの拡張子を変数に入れる
             $fileType_sub2 = pathinfo($subpath2,PATHINFO_EXTENSION);
-            if(!empty($_FILES['subPhoto1']['name'])){
+            if(!empty($_FILES[$subImg[1]]['name'])){
                 $allowTypes = array('jpg','png','jpeg','gif');
                 if(in_array($fileType,$allowTypes)){
                     if(move_uploaded_file($_FILES[$subImg[1]]['tmp_name'],"../". $path)){
                         //画像ファイルかのチェック
-                        if (exif_imagetype("../".$path)) {
-
-                        } else {
-
+                        if (!exif_imagetype("../".$path)) {
+                            if (!empty($_SERVER['HTTP_REFERER'])){
+                                header("Location:". $_SERVER['HTTP_REFERER']);
+                            }
                         }
                     }
                 }
@@ -81,28 +83,97 @@
             $fileName_sub1 = basename($_FILES[$subImg[2]]['name']);
             //二つをドッキング
             $subpath3 = $sub . $fileName_sub3;
+            // 画像パスの拡張子を変数に入れる
             $fileType_sub3 = pathinfo($subpath3,PATHINFO_EXTENSION);
+            if(!empty($_FILES[$subImg[2]]['name'])){
+                $allowTypes = array('jpg','png','jpeg','gif');
+                if(in_array($fileType,$allowTypes)){
+                    if(move_uploaded_file($_FILES[$subImg[2]]['tmp_name'],"../". $path)){
+                        //画像ファイルかのチェック
+                        if (!exif_imagetype("../".$path)) {
+                            if (!empty($_SERVER['HTTP_REFERER'])){
+                                header("Location:". $_SERVER['HTTP_REFERER']);
+                            }
+                        }
+                    }
+                }
+            }
         }
             
-            
-
-       if($_POST['subPhoto1']&&$_POST['subPhoto2']&&$_POST['subPhoto3']){
-
-       }else if($_POST['subPhoto1']){
-
-            
-       }
         $select='select user_id from user mail=?';
-        $insert='insert into profile values (null,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
-
         $id = $pdo->prepare($select);
         $id->execute($_SESSION['user']['id']);
-        $sql=$pdo->prepare($insert);
-        $sql->execute([
-            $id,$_POST['selfIntro'],$_POST['hobbies'],$_POST['gender'],$_POST['age'],$_POST['bloodType'],
-            $_POST['school'],$_POST['hometown'],$_POST['residence'],$_POST['message'],$_POST['profileIcon'],
-            $_POST['subPhoto1'],$_POST['subPhoto2'],$_POST['subPhoto3'],$drinking,$smoking
-        ]);
+
+        // 全てのサブ写真が設定されている場合
+        if($_POST['subPhoto1']&&$_POST['subPhoto2']&&$_POST['subPhoto3']){
+            $insert='insert into profile values (null,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
+            $sql=$pdo->prepare($insert);
+            $sql->execute([
+                $id,$_POST['selfIntro'],$_POST['hobbies'],$_POST['gender'],$_POST['age'],$_POST['bloodType'],
+                $_POST['school'],$_POST['hometown'],$_POST['residence'],$_POST['message'],$_POST['profileIcon'],
+                $_POST['subPhoto1'],$_POST['subPhoto2'],$_POST['subPhoto3'],$drinking,$smoking
+            ]);
+        // 1つ目のサブ写真が設定されている場合
+        }else if($_POST['subPhoto1']){
+            // 2つ目のサブ写真が設定されている場合
+            if($_POST['subPhot2']){
+                $insert='insert into profile values (null,?,?,?,?,?,?,?,?,?,?,?,?,?,null,?,?)';
+                $sql=$pdo->prepare($insert);
+                $sql->execute([
+                    $id,$_POST['selfIntro'],$_POST['hobbies'],$_POST['gender'],$_POST['age'],$_POST['bloodType'],
+                    $_POST['school'],$_POST['hometown'],$_POST['residence'],$_POST['message'],$_POST['profileIcon'],
+                    $_POST['subPhoto1'],$_POST['subPhoto2'],$drinking,$smoking
+                ]);
+            // 2つ目のサブ写真が設定されている場合
+            }else if($_POST['subPhot3']){
+                $insert='insert into profile values (null,?,?,?,?,?,?,?,?,?,?,?,?,null,?,?,?)';
+                $sql=$pdo->prepare($insert);
+                $sql->execute([
+                    $id,$_POST['selfIntro'],$_POST['hobbies'],$_POST['gender'],$_POST['age'],$_POST['bloodType'],
+                    $_POST['school'],$_POST['hometown'],$_POST['residence'],$_POST['message'],$_POST['profileIcon'],
+                    $_POST['subPhoto1'],$_POST['subPhoto3'],$drinking,$smoking
+                ]);
+            }
+            // 1つ目のサブ写真のみが設定されている場合
+            $insert='insert into profile values (null,?,?,?,?,?,?,?,?,?,?,?,?,null,null,?,?)';
+            $sql=$pdo->prepare($insert);
+            $sql->execute([
+                $id,$_POST['selfIntro'],$_POST['hobbies'],$_POST['gender'],$_POST['age'],$_POST['bloodType'],
+                $_POST['school'],$_POST['hometown'],$_POST['residence'],$_POST['message'],$_POST['profileIcon'],
+                $_POST['subPhoto1'],$drinking,$smoking
+            ]);
+        // 2つ目のサブ写真が設定されている場合
+        }else if($_POST['subPhoto2']){
+            // 3つ目のサブ写真が設定されている場合
+            if($_POST['subPhot3']){
+                $insert='insert into profile values (null,?,?,?,?,?,?,?,?,?,?,?,null,?,?,?,?)';
+                $sql=$pdo->prepare($insert);
+                $sql->execute([
+                    $id,$_POST['selfIntro'],$_POST['hobbies'],$_POST['gender'],$_POST['age'],$_POST['bloodType'],
+                    $_POST['school'],$_POST['hometown'],$_POST['residence'],$_POST['message'],$_POST['profileIcon'],
+                    $_POST['subPhoto2'],$_POST['subPhoto3'],$drinking,$smoking
+                ]);
+            }
+            // 1つ目のサブ写真のみが設定されている場合
+            $insert='insert into profile values (null,?,?,?,?,?,?,?,?,?,?,?,null,?,null,?,?)';
+            $sql=$pdo->prepare($insert);
+            $sql->execute([
+                $id,$_POST['selfIntro'],$_POST['hobbies'],$_POST['gender'],$_POST['age'],$_POST['bloodType'],
+                $_POST['school'],$_POST['hometown'],$_POST['residence'],$_POST['message'],$_POST['profileIcon'],
+                $_POST['subPhoto2'],$drinking,$smoking
+            ]);
+        // 3つ目のみに画像が設定されている場合
+        }else{
+            $insert='insert into profile values (null,?,?,?,?,?,?,?,?,?,?,?,null,null,?,?,?)';
+            $sql=$pdo->prepare($insert);
+            $sql->execute([
+                $id,$_POST['selfIntro'],$_POST['hobbies'],$_POST['gender'],$_POST['age'],$_POST['bloodType'],
+                $_POST['school'],$_POST['hometown'],$_POST['residence'],$_POST['message'],$_POST['profileIcon'],
+                $_POST['subPhoto3'],$drinking,$smoking
+            ]);
+        }
+        
+        
         foreach($sql as $row){
             $_SESSION['user']=[
                 'gender'=>$row['gender'],'age'=>$row['age'],'icon'=>$row['icon_image']
