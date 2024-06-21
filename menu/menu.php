bca468cc69279e936de2916f988d04860aeb4374
miyuu22 <139205386+miyuu22@users.noreply.github.com>
2024-06-21 11:06:56 +0900

    menu

--- menu/menu.php
+++ menu/menu.php
@@ -37,7 +37,7 @@
 
 
         <div class="link-space">
-            <p class="textlink textlink04"><img src="../menu-image/parson-free-icon.png" class="parson-free-icon"><a href="../profile/profile.php">プロフィール</a></p>
+            <p class="textlink textlink04"><img src="../menu-image/parson-free-icon.png" class="parson-free-icon"><a href="../profile/profile_up.php">プロフィール</a></p>
             <p class="textlink textlink04"><img src="../menu-image/seach-free-icon.png" class="seach-free-icon"><a href="../search/search.php">さがす</a></p>
             <p class="textlink textlink04"> <img src="../menu-image/like-free-icon.png" class="like-free-icon"><a href="../likes/likes.php">いいね</a></p>
             <p class="textlink textlink04"><img src="../menu-image/talk-free-icon.png" class="talk-free-icon"><a href="../talk/talk_top.php">トーク</a></p>
