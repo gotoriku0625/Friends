40686a2cee35d9b25e9f105ddc1329a8791fb525
harada93 <148171467+harada93@users.noreply.github.com>
2024-06-21 12:42:23 +0900

    k

--- m_menu/m_menu.php
+++ m_menu/m_menu.php
@@ -20,7 +20,7 @@
 
 
         <div class="link-space">
-            <p class="textlink textlink04"><a href="../management/Dashboard.php"></a></p>ダッシュボード
+            <p class="textlink textlink04"><a href="../management/Dashboard.php">ダッシュボード</a></p>
             <p class="textlink textlink04"><a href="../management/user_ichiran.php">ユーザー一覧</a></p>
             <p class="textlink textlink04"><a href="../management/tuuhou.php">通報一覧</a></p>
             <p class="textlink textlink04"><a href="../management/kanrisutaffu_settei.php">管理者スタッフ設定</a></p>
