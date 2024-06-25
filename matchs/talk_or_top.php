6a7f43a66a36f0a5b2935a69d39b6a124b78615b
gotoriku0625 <2201147@s.asojuku.ac.jp>
2024-06-25 12:42:54 +0900

    マッチングまだ出来てないです

--- matchs/talk_or_top.php
+++ matchs/talk_or_top.php
@@ -6,24 +6,28 @@ $pdo = new PDO($connect, USER, PASS);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // ユーザーIDはセッションから取得
-$sender_id = $_SESSION['user']['id'];
-$reciver_id = $_POST['reciver_id'];
+$logged_in_user_id = $_SESSION['user']['id'];
+$matched_user_id = $_POST['reciver_id'];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $action = $_POST['action'];
 
-    // talk_memberに登録する
-    $stmt = $pdo->prepare("INSERT INTO talk_member (sender_id, reciver_id) VALUES (:sender_id, :reciver_id)");
-    $stmt->bindParam(':sender_id', $sender_id);
-    $stmt->bindParam(':reciver_id', $reciver_id);
-    $stmt->execute();
+    try {
+        // talk_memberに登録する
+        $sql = "INSERT INTO talk_member (sender_id, reciver_id) VALUES (?, ?), (?, ?)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$logged_in_user_id, $matched_user_id, $matched_user_id, $logged_in_user_id]);
 
-    // 遷移先を設定
-    if ($action == 'talk') {
-        header("Location: talk.php?reciver_id=$reciver_id");
-    } else {
-        header("Location: top.php");
+        // 遷移先を設定
+        if ($action == 'talk') {
+            header("Location: talk.php?reciver_id=$matched_user_id");
+        } else {
+            header("Location: top.php");
+        }
+        exit;
+    } catch (PDOException $e) {
+        // エラー処理
+        echo "エラー: " . $e->getMessage();
     }
-    exit;
 }
 ?>
