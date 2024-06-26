a6e63d83bacedcb070c7dfa1411abe26d308caa9
soshi09 <2201208@s.asojuku.ac.jp>
2024-06-26 11:27:17 +0900

    a

--- search/result.php
+++ search/result.php
@@ -1,7 +1,5 @@
 <?php require '../header.php';?>
-<link rel="stylesheet" href="./top.css">
-<link rel="stylesheet" href="../menu/menu.css">
-<link rel="stylesheet" href="result.css">
+
 <title>検索結果</title>
 </head>
 
@@ -13,6 +11,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./top.css">
+    <link rel="stylesheet" href="../menu/menu.css">
+    <link rel="stylesheet" href="result.css">
     <title>検索結果</title>
 </head>
 <body>
