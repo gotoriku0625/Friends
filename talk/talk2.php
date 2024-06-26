aaa668f356e25de6fd53ca38309b0e54fb99cf06
koko2025 <sahakoke@icloud.com>
2024-06-26 12:45:35 +0900

    talk

--- talk/talk2.php
+++ talk/talk2.php
@@ -114,17 +114,6 @@ $messages = get_talks($current_user['user_id'],$reciver['user_id']);// やり取
         </div>
 
     </div>
-    <!-- モーダルの表示(ポップアップ表示)　ブロック -->
-            <section id="block">
-                <!-- <h3>ブロックしますか？</h2> -->
-                <form action="./talk2-add.php" method="post">
-                    <button type="submit" name="check" value="1">はい</button>
-                    <button type="submit" name="check" value="0">いいえ</button>
-                </form>
-            </section>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Modaal/0.4.4/js/modaal.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js"></script>
 </body>
 
 
