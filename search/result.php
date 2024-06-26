9c238dad582325457c4d3e8679b42ccf7822f2d2
soshi09 <2201208@s.asojuku.ac.jp>
2024-06-26 11:29:09 +0900

    a

--- search/result.php
+++ search/result.php
@@ -11,7 +11,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./top.css">
     <link rel="stylesheet" href="../menu/menu.css">
     <link rel="stylesheet" href="result.css">
     <title>検索結果</title>
