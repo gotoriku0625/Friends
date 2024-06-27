3fec0dac668cebcf8c6836683002bc9ac27a36ab
miyuu22 <139205386+miyuu22@users.noreply.github.com>
2024-06-27 12:36:34 +0900

    top

--- top/top.php
+++ top/top.php
@@ -45,10 +45,11 @@
         echo '</div>';
 
         // ランダムに30人を表示する
-        $sql=$pdo->prepare('select user.user_id, icon_image, user_name, gender_name, age 
-                            from user
+        $sql=$pdo->prepare('select user.user_id, icon_image, user_name, gender_name, age, profile.user_id
+                            from user profile
                             join profile ON user.user_id = profile.user_id
                             join gender ON gender.gender_id = profile.gender_id
+                            where profile.user_id<>?
                             order by RAND()
                             limit 30');
         $sql->execute();
