be922efe8040f1ffade09eeb317dbf313790d6f5
miyuu22 <139205386+miyuu22@users.noreply.github.com>
2024-06-28 09:48:48 +0900

    talk

--- talk/talk2.php
+++ talk/talk2.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
     <link rel="stylesheet" href="../menu/menu.css">
     <link rel="stylesheet" href="./talk.css">
     <script type="module" src="./script.js"></script>
