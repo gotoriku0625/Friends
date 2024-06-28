f00f6673bba3dd7eb0e39cebe7e15a365f9f5cf0
miyuu22 <139205386+miyuu22@users.noreply.github.com>
2024-06-28 09:54:50 +0900

    talk

--- talk/talk2.php
+++ talk/talk2.php
@@ -1,10 +1,4 @@
-<?php session_start(); ?>
-<?php require '../db-connect.php';?>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
-    <link rel="stylesheet" href="../menu/menu.css">
+<?php require '../header.php';?>
     <link rel="stylesheet" href="./talk.css">
     <script type="module" src="./script.js"></script>
     <title>talk</title>
