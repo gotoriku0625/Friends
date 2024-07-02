7c355627f9711be5c894af8e984cc3e952dcbf4d
Shugo0611 <2201171@s.asojuku.ac.jp>
2024-07-02 11:33:32 +0900

    プロフィール

--- profile/profile_up.php
+++ profile/profile_up.php
@@ -1,7 +1,7 @@
 <?php require '../header.php';?>
 <?php require '../menu/menu.php';?><!--menuはbodyタグの中に絶対に入れるように -->
 <title>プロフィール</title>
-<link rel="stylesheet" href="profile.css">
+<link rel="stylesheet" href="css/profile.css">
 <script src="js/style.js"></script>
 </head>
 <!-- プロフィールを更新するためのもの -->
