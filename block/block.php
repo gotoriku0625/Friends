6276a10e7d9b5dc6e9075ddd9674a1fb34196c89
koko2025 <sahakoke@icloud.com>
2024-07-03 12:24:06 +0900

    talk/あとは通報とトークルーム削除のみ

--- block/block.php
+++ block/block.php
@@ -5,4 +5,8 @@ $pdo=new PDO($connect,USER,PASS);
 $block='insert into block values (null,?,?)';
 $sql=$pdo->prepare($block);
 $sql->execute(array($_SESSION['user']['id'],$_POST['reciver_id']));
+echo '<form name="add" action="./talk2.php" method="post">';
+    echo '<input type="hidden" name="reciver_id" value="'.$_POST['reciver_id'].'">';
+    echo '<SCRIPT language="JavaScript">document.add.submit();</SCRIPT>';
+echo '</form>';
 ?>
