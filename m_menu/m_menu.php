2621aa3beb09964a1da29afc64e33a029c1c033d
miyuu22 <139205386+miyuu22@users.noreply.github.com>
2024-07-03 10:15:41 +0900

    management

--- m_menu/m_menu.php
+++ m_menu/m_menu.php
@@ -3,22 +3,10 @@
         <div class="logo-space">
             <a href="../top/top.php"><img src="../image/logo.png" class="logo"></a>
         </div>
-        <?php
-
-           $username = $_SESSION['m_user_name'];
-                echo '<div class="name">',$username,'</div>';
-        
-            
-
-                // 出力UI確認用に一時置き返してます（後で戻す予定）
-                // $icon = "logo.png";
-                // 出力UI確認用に一時置き返してます（後で戻す予定）
-                //$username = $_SESSION['m_user_id'];
-                //$username = $_SESSION['m_user_name'];
-                // $username = "sample";
-                //echo '<div class="name">',$username,'</div>';
-        ?>
-
+            <?php
+            $username = $_SESSION['m_user_name'];
+            echo '<div class="name">',$username,'</div>';
+            ?>
 
         <div class="link-space">
             <p class="textlink textlink04"><a href="../management/Dashboard.php">ダッシュボード</a></p>
