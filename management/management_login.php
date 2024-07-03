a575a743051e85684b0156cf3ce3a765414d11bf
miyuu22 <139205386+miyuu22@users.noreply.github.com>
2024-07-03 11:19:12 +0900

    management

--- management/management_login.php
+++ management/management_login.php
@@ -1,36 +1,5 @@
 <?php session_start(); ?>
 <?php require '../db-connect.php'; ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="css/management_login.css">
-</head>
-<div class="container">
-    <div class="header">
-        <img src="../image/logo.png" alt="Logo" class="logo">
-    </div>
-    <div class="content">
-        <img src="../image/person1.png" alt="human1" class="side-image">
-        <form action="" method="post" class="login-form">
-            <div class="form-group">
-                <label for="id">ID</label>
-                <input type="text" id="id" name="id">
-            </div>
-            <div class="form-group">
-                <label for="password">Pass</label>
-                <input type="password" id="password" name="password">
-            </div>
-            <?php if (isset($error_message)): ?>
-                <p><?= htmlspecialchars($error_message) ?></p>
-            <?php endif; ?>
-            <button type="submit" name="login" value="ログイン">ログイン</button>
-        </form>
-        <img src="../image/person2.png" alt="human2" class="side-image">
-    </div>
-    <div class="footer">
-    </div>
     <?php
         try {
             $pdo = new PDO($connect, USER, PASS);
@@ -56,6 +25,31 @@
         }
     ?>
     
+<body>
+<div class="container">
+    <div class="header">
+        <img src="../image/logo.png" alt="Logo" class="logo">
+    </div>
+    <div class="content">
+        <img src="../image/person1.png" alt="human1" class="side-image">
+        <form action="" method="post" class="login-form">
+            <div class="form-group">
+                <label for="id">ID</label>
+                <input type="text" id="id" name="id">
+            </div>
+            <div class="form-group">
+                <label for="password">Pass</label>
+                <input type="password" id="password" name="password">
+            </div>
+            <?php if (isset($error_message)): ?>
+                <p><?= htmlspecialchars($error_message) ?></p>
+            <?php endif; ?>
+            <button type="submit" name="login" value="ログイン">ログイン</button>
+        </form>
+        <img src="../image/person2.png" alt="human2" class="side-image">
+    </div>
+    <div class="footer">
+    </div>
 </div>
 </body>
 </html>
