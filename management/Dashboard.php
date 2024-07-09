b01d6c40fb9c9c0ad1d4494c43e5f1ae831ce93a
miyuu22 <139205386+miyuu22@users.noreply.github.com>
2024-07-09 11:35:29 +0900

    Dash

--- management/Dashboard.php
+++ management/Dashboard.php
@@ -72,9 +72,10 @@
         </div>
 
                 <div class="dashboard-item">
-                    <p>通報数：<?php echo $report_count; ?></p>
-                    <a href="tuuhou_ichiran.php" class="itiranGo1">通報一覧へ</a>
-                </div>
+            <ul>
+                <?php foreach ($category_counts as $category => $count) : ?>
+                    <li><?php echo htmlspecialchars($category); ?>: <?php echo $count; ?>件</li>
+            <a href="tuuhou_ichiran.php" class="itiranGo1">通報一覧へ</a>
 
                 <div class="dashboard-item">
                     <p>ブロック数：<?php echo $block_count; ?></p>
