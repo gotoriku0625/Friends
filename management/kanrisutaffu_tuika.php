db2d958624f2f34c647fc21ed9175c3766a8ad8d
soshi09 <2201208@s.asojuku.ac.jp>
2024-07-10 12:27:16 +0900

    リンクミスの修正

--- management/kanrisutaffu_tuika.php
+++ management/kanrisutaffu_tuika.php
@@ -42,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <link rel="stylesheet" href="css/kanrisutaffu_settei.css">
+    <link rel="stylesheet" href="css/kanrisutaffu_tuika.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>管理スタッフ設定</title>
