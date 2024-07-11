569dcd67b6587b0b0b513ac861ec151d75d61f66
Shugo0611 <2201171@s.asojuku.ac.jp>
2024-07-11 11:53:12 +0900

    ログアウトｃｓｓ

--- logout/logout.php
+++ logout/logout.php
@@ -1,22 +1,24 @@
-<?php session_start();?>
-<?php require '../db-connect.php';?>
 <!DOCTYPE html>
 <html lang="jp">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/logout.css">
+    <title>ログアウト</title>
+    <link rel="stylesheet" href="css/logout.css"> <!-- logout.cssファイルのリンク -->
 </head>
 <body>
 <?php
 if (isset($_SESSION['user'])) {
     unset($_SESSION['user']);
-    echo '<div class="message">ログアウトしました。</div>';
+    echo '<div id="center">';
+    echo '<p class="message">ログアウトしました。</p>';
     echo '<a href="../login/login.html" class="btn">ログインへ</a>';
+    echo '</div>';
 } else {
-    echo '<div class="message">すでにログアウトしています。</div>';
+    echo '<div id="center">';
+    echo '<p class="message">すでにログアウトしています。</p>';
     echo '<a href="../login/login.html" class="btn">ログインへ</a>';
+    echo '</div>';
 }
 ?>
 </body>
