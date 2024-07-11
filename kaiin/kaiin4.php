0d929fcc231f3d6cdd6d957121cbc3570991c5f5
Shugo0611 <2201171@s.asojuku.ac.jp>
2024-07-11 10:55:42 +0900

    会員ｃｓｓ

--- kaiin/kaiin4.php
+++ kaiin/kaiin4.php
@@ -1,4 +1,10 @@
-<?php require '../header.php';?>
+<?php session_start(); ?>
+<?php require '../db-connect.php';?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- reset.css destyle -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
     <link rel="stylesheet" href="css/kaiin4.css">
     <title>新規会員登録</title>
 </head>
