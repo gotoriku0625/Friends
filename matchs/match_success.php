f4103a71c403f34c7b1ad77526474f6330243e89
miyuu22 <139205386+miyuu22@users.noreply.github.com>
2024-07-19 12:34:17 +0900

    mach

--- matchs/match_success.php
+++ matchs/match_success.php
@@ -37,7 +37,6 @@ $sql = 'SELECT u.user_name, p.icon_image ,p.gender_id ,p.age
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':reciver_id', $reciver_id, PDO::PARAM_INT);
 $stmt->execute();
-echo var_dump($sql);
 $receiver_profile = $stmt->fetch(PDO::FETCH_ASSOC);
 
 if ($receiver_profile) {
