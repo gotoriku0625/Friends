60b9edefd8feb5fcc8c1125c3678210c31c69875
koko2025 <sahakoke@icloud.com>
2024-07-23 21:35:56 +0900

    talk/無事終わり

--- report/report.php
+++ report/report.php
@@ -1,18 +1,33 @@
-<?php
+<?php 
+session_start();
 require '../db-connect.php';
-
+$user_id=$_SESSION['user']['id'];
+$reciver_id= $_SESSION['reciver'];
 $pdo=new PDO($connect,USER,PASS);
-// 通報
-$report='insert into report values (null,?,?,?,?)';
-$sql=$pdo->prepare($report);
-$sql->execute(array($_POST['user_id'],$_POST['reciver_id'],$_POST['report'],$_POST['re_text']));
-// ブロック
-$block='insert into block values(null,?,?)';
-$sql=$pdo->prepare($block);
-$sql->execute([$_POST['user_id'],$_POST['reciver_id']]);
+if(isset($_POST['check'])&&$_POST['check']==='block'){
+    $block='insert into block values(null,?,?)';
+    $sql=$pdo->prepare($block);
+    $sql->execute([$user_id,$reciver_id]);
 
-echo '<form name="report" action="../talk/talk2.php" method="post">';
-    echo '<input type="hidden" name="reciver_id" value="'.$_POST['reciver_id'].'">';
-    echo '<SCRIPT language="JavaScript">document.report.submit();</SCRIPT>';
-echo '</form>';
+    echo '<form name="block" action="../talk/talk2.php" method="post">';
+        echo '<input type="hidden" name="reciver_id" value="'.$reciver_id.'">';
+        unset($_SESSION['reciver']);
+        echo '<SCRIPT language="JavaScript">document.block.submit();</SCRIPT>';
+    echo '</form>';
+}else if(isset($_POST['check'])&&$_POST['check']==='report'){
+    // 通報
+    $report='insert into report values (null,?,?,?,?)';
+    $sql=$pdo->prepare($report);
+    $sql->execute(array($_POST['user_id'],$_POST['reciver_id'],$_POST['report'],$_POST['re_text']));
+    // ブロック
+    $block='insert into block values(null,?,?)';
+    $sql=$pdo->prepare($block);
+    $sql->execute([$_POST['user_id'],$_POST['reciver_id']]);
+
+    echo '<form name="report" action="../talk/talk2.php" method="post">';
+        echo '<input type="hidden" name="reciver_id" value="'.$_POST['reciver_id'].'">';
+        echo '<SCRIPT language="JavaScript">document.report.submit();</SCRIPT>';
+    echo '</form>';
+}
+
 ?>
